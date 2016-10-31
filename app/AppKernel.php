e626a19b6382b749329cce01decd128e9a8f5cc1
Kayneth <dylan.temboucti@gmail.com>
2016-10-31 22:36:44 +0100

    ended test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,7 +9,6 @@ class AppKernel extends Kernel
         parent::construct($environment, $debug);
         // get rid of Warning: date_default_timezone_get(): It is not safe to rely on the system's timezone
         date_default_timezone_set( 'Europe/Paris' );
-        //test v/.2
     }
 
     public function registerBundles()
