21c817c952dd365c0be738e04f92fa0d19e22a26
Dylan Temboucti <dylan.temboucti@gmail.com>
2016-12-12 14:29:22 +0100

    Create ContactType.php

---
+++ src/AppBundle/Form/ContactType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', null, array(
+                'label' => 'Votre nom'
+            ))
+            ->add('email', null, array(
+                'label' => 'Votre e-mail'
+            ))
+            ->add('subject', null, array(
+                'mapped' => false,
+                'label' => 'Sujet'
+            ))
+            ->add('message', TextareaType::class, array(
+                'mapped' =>false
+            ))
+            ->add('Envoyer', SubmitType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Shiawa\UserBundle\Entity\User'
+        ));
+    }
+}
+
+
