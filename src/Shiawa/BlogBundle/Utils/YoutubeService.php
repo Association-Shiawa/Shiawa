ab5d84754123902941f15e4d9b3be5ae7a68e46c
Kayneth <dylan.temboucti@gmail.com>
2017-01-16 00:48:02 +0100

    api youtube data test 2

--- src/Shiawa/BlogBundle/Utils/YoutubeService.php
+++ src/Shiawa/BlogBundle/Utils/YoutubeService.php
@@ -8,21 +8,24 @@ class YoutubeService {
     private $client;
     private $youtube;
     private $channelID;
+    //PlaylistID for uploaded videos
+    private $uploadsID;
 
-    public function __construct($apiKey, $channelID)
+    public function __construct($apiKey, $channelID, $uploadsID)
     {
         $this->apiKey = $apiKey;
         $this->client = new \Google_Client();
         $this->client->setDeveloperKey($this->apiKey);
         $this->youtube = new \Google_Service_YouTube($this->client);
         $this->channelID = $channelID;
+        $this->uploadsID = $uploadsID;
     }
 
     public function getLastVideos()
     {
-        $channelInfos = $this->youtube->channels->listChannels("snippet, contentDetails", array('id' => $this->channelID));
-        var_dump($channelInfos);die();
-//        $videos =
+        $videos = $this->youtube->playlistItems->listPlaylistItems('contentDetails', array('playlistId' => $this->uploadsID));
+        //channels->listChannels("snippet, contentDetails", array('id' => $this->channelID));
+        var_dump($videos['items'][0]['videoId']);die();
         return $videos;
     }
 
