f0f849ba081b32428941e4e53272327a637c8b69
kayneth <dylan.temboucti@gmail.com>
2017-11-05 22:06:07 +0100

    Mise à jour

---
+++ web/resources/src/js/shiawa_functions.js
@@ -0,0 +1,103 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var shiawa_module = (function () {
+
+    $(document).ready(function () {
+        $(".button-collapse").sideNav();
+        $(".dropdown-button").dropdown({constrain_width: false, hover: false, beloworigin: true});
+    });
+
+    self.transparentNavbar = function () {
+        var navbarContainer = document.querySelector(".transparent-navbar");
+        if(navbarContainer)
+        {
+            var navbar = navbarContainer.firstElementChild;
+            var nav = navbar.firstElementChild;
+            var navWrapper = nav.firstElementChild;
+
+            navWrapper.classList.add('transparent');
+            nav.classList.add('z-depth-0');
+            navWrapper.classList.remove('grey');
+            document.querySelector('.brand-logo').classList.add('hide');
+        }
+    };
+
+    self.coloredNavbar = function () {
+        var navbarContainer = document.querySelector(".transparent-navbar");
+        if(navbarContainer)
+        {
+            var navbar = navbarContainer.firstElementChild;
+            var nav = navbar.firstElementChild;
+            var navWrapper = nav.firstElementChild;
+
+            navWrapper.classList.remove('transparent');
+            nav.classList.remove('z-depth-0');
+            navWrapper.classList.add('grey');
+            document.querySelector('.brand-logo').classList.remove('hide');
+        }
+    };
+
+    self.scrolledNavbar = function () {
+        var position = $(window).scrollTop(); // should start at 0
+
+        if(position < 100 )
+        {
+            self.transparentNavbar();
+        }
+
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            if (position > 100) {
+                self.coloredNavbar();
+            } else {
+                self.transparentNavbar();
+            }
+            position = scroll;
+        });
+    }();
+
+    self.clickScroll = function () {
+        $('.mouse').click(function () {
+            $('html, body').animate({
+                scrollTop: $(".articles").offset().top - 150
+            }, 1500);
+        });
+    }();
+
+    self.carouselShiawa = function () {
+        $(document).ready(function () {
+            $('.carousel').carousel({
+                dist: 0,
+                padding: 40,
+                fullWidth: true
+            });
+        });
+    }();
+
+    self.slider = function () {
+        $(document).ready(function () {
+            $('.slider').slider({full_width: true, height: 600});
+        });
+    }();
+
+    self.initMaterialboxed = function () {
+        $(document).ready(function () {
+            $('.materialboxed').materialbox();
+        });
+    }();
+
+    self.initMaterialSelect = function () {
+        $(document).ready(function() {
+            $('select').material_select();
+        });
+    }();
+
+    self.initCarousel = function () {
+        $('.carousel.carousel-slider').carousel({full_width: true});
+    }();
+
+})();
