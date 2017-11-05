f1b4d1d5bb971714b0fe6d23ea8ddf037c2b703e
kayneth <dylan.temboucti@gmail.com>
2017-11-06 00:52:41 +0100

    Ajout de Wepack Encore + config React avec TypeScript en préparation

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // will output as web/build/app.js
+    .addEntry('app', './assets/js/main.js')
+
+    // will output as web/build/global.css
+    .addStyleEntry('global', './assets/scss/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader(function(sassOptions) {})
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+    // .autoProvideVariables({
+    //     $: 'jquery',
+    //     jQuery: 'jquery',
+    //     'window.jQuery': 'jquery',
+    // })
+    .enableReactPreset()
+    // Enable TypeScript
+    // .enableTypeScriptLoader(function(tsConfig) {})
+    .enablePostCssLoader()
+    .enableSourceMaps(!Encore.isProduction())
+    // create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
